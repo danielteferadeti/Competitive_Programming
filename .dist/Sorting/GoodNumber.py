62856369abaf0804b14c54c50614168180b0e263
danielteferadeti <danieltefera2211@gmail.com>
2021-12-10 09:28:50 +0300

    God Number Commited

---
+++ .dist/Sorting/GoodNumber.py
@@ -0,0 +1,7 @@
+def numIdenticalPairs(self, nums: List[int]) -> int:
+        good_prs = 0
+        for i in range(len(nums)):
+            for j in range(i+1, len(nums)):
+                if nums[i] == nums[j]:
+                    good_prs +=1
+        return good_prs
