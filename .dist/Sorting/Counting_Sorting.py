39d600e402139855d3ec48cd5a6158e59fceaf59
danielteferadeti <56646941+danielteferadeti@users.noreply.github.com>
2021-12-11 15:03:38 +0300

    Update Counting_Sorting.py

--- .dist/Sorting/Counting_Sorting.py
+++ .dist/Sorting/Counting_Sorting.py
@@ -1,20 +1,43 @@
-def counting_sort(given_list):
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'countingSort' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def countingSort(arr):
+    # Write your code here
+    given_list = arr
     count_list = []
     result = []
-    max_num = given_list[0]
-    for element in given_list:
-        if max_num < element:
-            max_num = element
+    max_num = 100
 
-    for j in range(max_num + 1):
+    for j in range(max_num ):
         count_list.append(0)
 
     for elt in given_list:
         count_list[elt] +=1
 
-    for i in range(len(count_list)):
-        if count_list[i] !=0:
-            for k in range(count_list[i]):
-                result.append(i)
-    return result
+    return count_list
 
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = countingSort(arr)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
