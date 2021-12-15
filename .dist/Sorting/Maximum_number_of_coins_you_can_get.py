c9cc28be12414542c2dea78b50fa5bc1cab38511
danielteferadeti <danieltefera2211@gmail.com>
2021-12-15 19:11:55 +0300

    Maximum Coins you can get commited.

---
+++ .dist/Sorting/Maximum_number_of_coins_you_can_get.py
@@ -0,0 +1,11 @@
+class Solution:
+    def maxCoins(self, piles: List[int]) -> int:
+        piles.sort()
+        max_coins = 0
+        i = len(piles) -2
+        
+        while i > int(len(piles)/3) -1:
+            max_coins += piles[i]
+            i -=2
+            
+        return max_coins
