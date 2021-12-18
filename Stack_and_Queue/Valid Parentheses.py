4410ae187b23ecccff5984b559b4cea993096106
danielteferadeti <danieltefera2211@gmail.com>
2021-12-18 13:27:35 +0300

    Impilmenting queue uding stack commited.

--- Stack_and_Queue/Valid Parentheses.py
+++ Stack_and_Queue/Valid Parentheses.py
@@ -1,1 +1,26 @@
 #Valid Parentheses
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        
+        if len(s) < 2:
+            return False 
+        
+        for char in s:
+            if char == '(' or char=='{' or char=='[':
+                stack.append(char)
+            else:
+                if len(stack)==0:
+                    return False
+                from_stack = stack.pop()
+                if from_stack == '(' and char != ')':
+                    return False
+                elif from_stack == '[' and char != ']':
+                    return False 
+                elif from_stack =='{' and char != '}':
+                    return False
+                
+        if len(stack)>0:
+            return False 
+        
+        return True
