4053dad156e077cb5195b9c37f16a9e87e21567f
danielteferadeti <danieltefera2211@gmail.com>
2022-07-14 00:44:12 +0300

    .

--- Daily Questions/1557. Minimum Number of Vertices to Reach All Nodes.py
+++ Daily Questions/1557. Minimum Number of Vertices to Reach All Nodes.py
@@ -6,7 +6,7 @@ class Solution:
         for edge in edges:
             begin = edge[0]
             end = edge[1]
-            not_include.add(end)            
+            not_include.add(end)
             include.add(begin)
 
         for i in not_include:
