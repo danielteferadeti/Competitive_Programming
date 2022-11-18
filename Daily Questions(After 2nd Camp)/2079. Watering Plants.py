9ecbd371ef2ef018f473ceeafa1d04f52f23b198
danielteferadeti <danieltefera2211@gmail.com>
2022-11-18 10:03:30 +0300

    2079. Watering Plants solved

---
+++ Daily Questions(After 2nd Camp)/2079. Watering Plants.py
@@ -0,0 +1,18 @@
+class Solution:
+    def wateringPlants(self, plants: List[int], capacity: int) -> int:
+        total = 1
+        original = capacity
+        capacity -= plants[0]
+        
+        for i in range(1,len(plants)):
+            waterNeeded = plants[i]
+            
+            if waterNeeded <= capacity:
+                total += 1
+                capacity -= waterNeeded
+            else:
+                total += (((i+1)*2)-1)
+                capacity = original
+                capacity -= waterNeeded
+        
+        return total
