328035f32e61bffddbb281db9ae438d6e5bfd94a
danielteferadeti <danieltefera2211@gmail.com>
2023-02-01 20:45:47 +0300

    2136. Earliest Possible Day of Full Bloom solved.

---
+++ Daily Questions(After 2nd Camp)/squid game/2136. Earliest Possible Day of Full Bloom.py
@@ -0,0 +1,13 @@
+class Solution:
+    def earliestFullBloom(self, plantTime: List[int], growTime: List[int]) -> int:
+        
+        sortArray = [(-growTime[i],plantTime[i],i) for i in range(len(plantTime))]
+        sortArray.sort()
+        
+        bestMax = 0
+        platingDays = 0
+        for i in range(len(plantTime)):
+            platingDays += plantTime[sortArray[i][2]]
+            bestMax = max(bestMax, platingDays+growTime[sortArray[i][2]])
+            
+        return bestMax
